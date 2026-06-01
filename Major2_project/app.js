4cc591d552ab09a483a993de2992e0adddbc02c7
docas <dorcas.lam@corp.atfx.com>
2026-06-01 18:05:37 +0800

    added localstorage to set and add data, also has format and display data

--- Major2_project/app.js
+++ Major2_project/app.js
@@ -1,14 +1,44 @@
 // grab the form - start from chrome dev tools to do this
+let stockportfolio = [];
+let isClicked = false;
 const todoForm = document.querySelector("#todo-form");
 
 const searchButton = document.querySelector(".add-btn");
 const searchResult = document.querySelector("#search-result");
+const portfolioSummary = document.querySelector("#portSummary");
 const todoInput = document.querySelector("#todo-input");
 const portSummary = document.querySelector("#portSummary");
+const addPortForm = document.querySelector("#addPort-form");
 const addPortButton = document.querySelector(".addPort-btn");
 const useFakeData = true;
-const addPortInput = document.querySelector("#addPort-input");
+const myportValue = document.querySelector(".myportValue");
+const portSummaryValue = document.querySelector(".PortSummaryValue");
+const myStocks = document.querySelector(".myStocks");
+// Todo Management Functions
+async function addStock(text) {
+  const addStockdata = await getRecipes(text);
+  const priceValue = addStockdata["Global Quote"]["05. price"];
+  const changeValue = addStockdata["Global Quote"]["09. change"];
+  const changePercentValue = addStockdata["Global Quote"]["10. change percent"];
+  const newstock = {
+    id: Date.now().toString(),
 
+    name: text,
+    priceValue: priceValue,
+    changeValue: changeValue,
+    changePercentValue: changePercentValue,
+    createdAt: new Date().toISOString(),
+  };
+
+  stockportfolio.push(newstock);
+
+  let data = localStorage.setItem(
+    "stockportfolio",
+    JSON.stringify(stockportfolio),
+  );
+
+  console.log("Added stock:", stockportfolio);
+}
 async function getRecipes(valueID) {
   if (!useFakeData) {
     // fetch code
@@ -50,10 +80,10 @@ async function getRecipes(valueID) {
 
 document.addEventListener("DOMContentLoaded", function () {
   // Form submission and eventhandling
+  loadStocksFromStorage();
   searchResult.classList.add("hidden");
 
   todoForm.addEventListener("submit", handleFormSubmit);
-  addPortButton.addEventListener("click", handlePortfolioSubmit);
 });
 // add event listener to the form
 async function handleFormSubmit(event) {
@@ -84,17 +114,21 @@ async function handleFormSubmit(event) {
               <input
                 type="text"
                 id="addPortInput"
-                class="addPortInput"
+                class="addPortInput "
                 placeholder="Number of shares"
                 required
                 aria-label="Add to Portfolio item"
               />
+               <button  onclick="handlePortfolioSubmit(event)" type="submit" class="addPort-btn" for="addPortInput" >Update</button>
               </form><br><br>
-              <button type="submit" class="addPort-btn" for="addPort-input"   >Update</button>
+             
       
       
     </div>
   `;
+
+  const addPortInput = document.querySelector("#addPortInput");
+
   if (stockId.toUpperCase() == datas["Global Quote"]["01. symbol"]) {
     const priceValue = datas["Global Quote"]["05. price"];
     const changeValue = datas["Global Quote"]["09. change"];
@@ -124,7 +158,42 @@ async function handleFormSubmit(event) {
 // add event listener to portfolio form
 function handlePortfolioSubmit(event) {
   event.preventDefault();
-  const stockShares = addPortInput.querySelector("#addPort-input").value;
-  console.log("Number of shares to update:", stockShares);
-  portSummary.textContent = stockShares + " shares added to portfolio.";
+
+  const stockShares = addPortInput.value;
+
+  addStock(todoInput.value);
+
+  const data = loadStocksFromStorage();
+  console.log("Data loaded from storage:", data);
+  let priceValues = 0;
+  data.map((stock) => {
+    if (stock.name === todoInput.value) {
+      priceValues = stock.priceValue;
+    }
+  });
+
+  const portHTML = `
+ 
+       <br><br>
+       <div class="PortSummaryHTML">
+              
+             Number of shares hold:
+              <span class="PortSummaryValue">${stockShares}</span>
+              <br> 
+                     Your current stock value is: $<span class="PortSummaryValue">${priceValues * stockShares}</span>
+         </div>
+  `;
+  portSummary.innerHTML += portHTML;
+
+  myportValue.textContent = priceValues * stockShares;
+  myStocks.textContent = stockShares;
+}
+function loadStocksFromStorage() {
+  // Try to get the saved stocks from browser storage
+  const storedStocks = localStorage.getItem("stockportfolio") || "[]"; // Default to an empty array if nothing is found
+  // Only try to parse if we actually found saved data
+  if (storedStocks) {
+    // Convert the JSON string back into an array
+    return (stockportfolio = JSON.parse(storedStocks));
+  }
 }
